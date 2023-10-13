15c211ee88cf999cdcc6c2b79546b45de4360b00
Geordyn <geordyata@aol.com>
2023-10-12 21:09:41 -0400

    first commit

---
+++ ContactManagerStarter/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagerStarter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error has occured on the homepage: " + ex.Message);
+            }
+        }
+    }
+}
